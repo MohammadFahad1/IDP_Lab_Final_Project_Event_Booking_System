3864c1800b4827c5c163501efb65a591031b4d51
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-12-09 23:51:53 +0600

    Requirements page [Done]

---
+++ src/pages/Requirements.jsx
@@ -0,0 +1,31 @@
+const Requirements = () => {
+  return (
+    <div className="bg-green-200 text-green-600 min-w-full min-h-[100vh]">
+      <h1 className="font-bold text-4xl text-center pt-8">
+        Event Booking System
+      </h1>
+      <h2 className="text-3xl font-thin text-center">Dynamic Pricing System</h2>
+      <div className="flex justify-center">
+        <div className="text-center mt-8 bg-white w-[500px] p-4">
+          <h2 className="text-3xl text-green-800 pb-3 leading-none font-bold underline">
+            Your Requirements
+          </h2>
+          <ul className="text-left text-2xl pb-5 leading-[2]">
+            <li># Lunch (Daal, Bhat, Mashed Potatoes)</li>
+            <li># Cold Drinks (Coke, Mojo, Sprite etc)</li>
+            <li># Sweets (Rasmalai, Kalojam, Doi)</li>
+            <li># Tables & Chairs (20 x 6)</li>
+          </ul>
+          <button
+            type="submit"
+            className="bg-green-600 text-white inline-block p-3 w-96 rounded-full border-solid border-2 border-green-800 hover:bg-green-900"
+          >
+            Confirm and Pay
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Requirements;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,11 +4,14 @@ import NotFound from "../components/NotFound";
 import Login from "../pages/Login";
 import Signup from "../pages/signup";
 import BookSchedule from "../pages/BookSchedule";
+import Requirements from "../pages/Requirements";
+
 const router = createBrowserRouter([
   { path: "/", element: <App></App> },
   { path: "/login", element: <Login></Login> },
   { path: "/signup", element: <Signup></Signup> },
   { path: "/eventscheduling", element: <BookSchedule></BookSchedule> },
+  { path: "/requirements", element: <Requirements></Requirements> },
   { path: "*", element: <NotFound></NotFound> },
 ]);
 
