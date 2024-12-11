ac272e99bd1ee1e60852a9c5eaee75638a52c3ef
MohammadFahad1 <fahad4bangladesh@gmail.com>
2024-12-11 21:57:55 +0600

    Requirements Page Created

--- src/pages/Requirements.jsx
+++ src/pages/Requirements.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 const Requirements = () => {
   return (
     <div className="bg-green-200 text-green-600 min-w-full min-h-[100vh]">
@@ -10,18 +12,21 @@ const Requirements = () => {
           <h2 className="text-3xl text-green-800 pb-3 leading-none font-bold underline">
             Your Requirements
           </h2>
-          <ul className="text-left text-2xl pb-5 leading-[2]">
+          <ul className="text-left text-2xl pb-3 leading-[2]">
             <li># Lunch (Daal, Bhat, Mashed Potatoes)</li>
             <li># Cold Drinks (Coke, Mojo, Sprite etc)</li>
             <li># Sweets (Rasmalai, Kalojam, Doi)</li>
             <li># Tables & Chairs (20 x 6)</li>
           </ul>
-          <button
-            type="submit"
+          <h1 className="text-4xl text-orange-500 pb-3 text-left font-extrabold">
+            Price: 1,25,000.00
+          </h1>
+          <Link
+            to="/payment"
             className="bg-green-600 text-white inline-block p-3 w-96 rounded-full border-solid border-2 border-green-800 hover:bg-green-900"
           >
             Confirm and Pay
-          </button>
+          </Link>
         </div>
       </div>
     </div>
